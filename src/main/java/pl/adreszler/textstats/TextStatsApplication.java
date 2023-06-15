39f5e58b830b2a066de5d29d58f6a02a77e197c8
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-15 18:08:37 +0200

    Project created

---
+++ src/main/java/pl/adreszler/textstats/TextStatsApplication.java
@@ -0,0 +1,13 @@
+package pl.adreszler.textstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/adreszler/textstats/TextStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.textstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
